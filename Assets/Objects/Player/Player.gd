9c216c166bc50d7091bd49b4aead812ea8ea46d8
Ondřej Černý <sprotex@gmail.com>
2021-06-12 02:10:37 +0200

    add basic cable connection drawing

--- Assets/Objects/Player/Player.gd
+++ Assets/Objects/Player/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+class_name PlayerBody
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -14,8 +16,7 @@ onready var sprite = $tmp_player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	get_node("/root/LevelManager").player_reference = self
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
